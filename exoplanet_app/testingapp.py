95f5f7f8446b78cd02e1a96d06d7ae7d315d1695
SulthanFaez <131573027+SulthanFaez@users.noreply.github.com>
2025-10-04 17:09:45 +0800

    Update testingapp.py

--- exoplanet_app/testingapp.py
+++ exoplanet_app/testingapp.py
@@ -21,9 +21,8 @@ def load_default_model():
 
 def load_tabpfn_model():
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    model_path = os.path.join(BASE_DIR, "tabpfn_exoplanet.pkl")  # your trained model
-    temp_model = joblib.load(model_path)
-    temp_model.device = torch.device("cpu")
+    model_path = os.path.join(BASE_DIR, "tabpfn_exoplanet.pth")  # your trained model
+    temp_model = torch.load(model_path, weights_only=False, map_location=torch.device("cpu")
     return temp_model
 
 # model = None
@@ -312,6 +311,7 @@ with tab3:
 
             except Exception as e:
                 st.error(f"Error while loading model or predicting: {e}")
+
 
 
 
